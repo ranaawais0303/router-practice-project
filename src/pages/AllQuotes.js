3573a9f350200f77be9d4d3a3b86453b7367db32
Rana_awais <z.manzoor@pioneerlogics.com>
2022-07-29 02:54:36 -0700

    wrap layout and show list of quote items

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Awais", text: "React Quote Practice" },
+  { id: "q2", author: "Sarmad", text: "React Quote Learning" },
+];
 const AllQuotes = (props) => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
