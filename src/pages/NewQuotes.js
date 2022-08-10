6e34dc11c321f84f3fbdfa6b187f2d5de0214de9
Rana_awais <z.manzoor@pioneerlogics.com>
2022-08-10 00:39:06 -0700

    useHistory programmatic navigation

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,8 +1,12 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuotes = (props) => {
+  const history = useHistory();
   const addQuoteHandler = (QuoteData) => {
     console.log(QuoteData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
